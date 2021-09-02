9b25626a8b4f21b0f99859ae3af9822b8eeba01c
Tasnim <tasnim.mostari@gmail.com>
2021-09-02 20:21:27 +0600

    picture added

--- js/app.js
+++ js/app.js
@@ -5,8 +5,7 @@ const search = () => {
     // console.log(searchText);
 
 
-
-    // manage api
+    // manage book api
     const url = `https://openlibrary.org/search.json?q=${searchText}`
     fetch(url)
         .then(res => res.json())
@@ -31,14 +30,17 @@ const searchResult = (books) => {
 
 
     else {
+        const error = document.getElementById('error');
+        error.style.display = 'none';
 
         books.forEach(book => {
+
             // console.log(book);
             const div = document.createElement('div');
             div.classList.add('col');
             div.innerHTML = `
         <div class="card h-100">
-            <img src="..." class="card-img-top" alt="...">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${book.title}</h5>
                 <p class="card-text">${book.author_name}</p>
