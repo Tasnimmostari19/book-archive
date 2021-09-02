dc89795a96eff2fb362b9df803da98bf0dc66144
Tasnim <tasnim.mostari@gmail.com>
2021-09-02 21:04:32 +0600

    again updated

--- js/app.js
+++ js/app.js
@@ -43,8 +43,9 @@ const searchResult = (books) => {
             <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${book.title}</h5>
-                <p class="card-text">${book.author_name}</p>
-                <p class="card-text">${book.first_publish_year}</p>
+                <p class="card-text">Author: ${book.author_name}</p>
+                <p class="card-text">Publisher: ${book.publisher}</p>
+                <p class="card-text">First publish year: ${book.first_publish_year}</p>
             </div>
         </div>
         `;
